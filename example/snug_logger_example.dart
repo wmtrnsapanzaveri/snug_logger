515ab26d3aebf9fe7d01b2f5906a5a9fb8176ccc
Sapan Zaveri <sapanz@webmobtech.com>
2024-01-22 18:14:41 +0530

    first commit

---
+++ example/snug_logger_example.dart
@@ -0,0 +1,6 @@
+import 'package:snug_logger/snug_logger.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/snug_logger_test.dart
@@ -0,0 +1,16 @@
+import 'package:snug_logger/snug_logger.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
