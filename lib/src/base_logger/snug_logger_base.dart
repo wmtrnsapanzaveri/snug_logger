2d780da14aeb40603a21b228cb0ef94a2e3caadc
Sapan Zaveri <sapanz@webmobtech.com>
2024-01-30 14:18:15 +0530

    comments added

--- lib/src/base_logger/snug_logger_base.dart
+++ lib/src/base_logger/snug_logger_base.dart
@@ -1,6 +1,7 @@
 import 'package:snug_logger/src/base_logger/log_handler.dart';
 import 'package:snug_logger/src/utlis/log_type.dart';
 
+/// SnugLog is a logger that provides a simple and extensible API to log messages.
 snugLog(
     [dynamic content = "Debug Message",
     LogType logType = LogType.debug,
